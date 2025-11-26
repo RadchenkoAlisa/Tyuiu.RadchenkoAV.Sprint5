f06e4204063f536dbb14e1994984d1561cf37911
alisa69 <aliska132007@gmail.ru>
2025-11-27 00:25:27 +0500

    Task3

---
+++ Tyuiu.RadchenkoAV.Sprint5.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Tyuiu.RadchenkoAV.Sprint5.Task3.V11.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint5.Task3.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.GetTempFileName();
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExsist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExsist);
+        }
+    }
+}
