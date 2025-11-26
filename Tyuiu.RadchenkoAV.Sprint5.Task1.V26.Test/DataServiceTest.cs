d998359088f783d876282c1b198519e822deec2a
alisa69 <aliska132007@gmail.ru>
2025-11-27 00:07:37 +0500

    Task1

---
+++ Tyuiu.RadchenkoAV.Sprint5.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Tyuiu.RadchenkoAV.Sprint5.Task1.V26.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint5.Task1.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Cloud Star\source\repos\Tyuiu.SenachevAV.Sprint5\Tyuiu.SenachevAV.Sprint5.Task1.V26\bin\Debug\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.RadchenkoAV.Sprint5.Task1.V26/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.RadchenkoAV.Sprint5.Task1.V26.Lib;
+int startValue = -5;
+int stopValue = -5;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #5 | Выполнил: РадченкоАВ | РППБ-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+Console.WriteLine("* Задание #1                                                              *");
+Console.WriteLine("* Вариант #26                                                             *");
+Console.WriteLine("* Выполнил: РадченкоАВ | РППБ-25-1                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дана функция, произвести табулирование на заданном диапазоне [-5;5]     *");
+Console.WriteLine("* с шагом 1.                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("startValue = " + startValue);
+Console.WriteLine("stopValue = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string res = ds.SaveToFileTextData(startValue, stopValue);
+
+Console.WriteLine("Файл " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
