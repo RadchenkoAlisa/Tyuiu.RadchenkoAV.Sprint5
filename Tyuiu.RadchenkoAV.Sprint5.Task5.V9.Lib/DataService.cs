ee26c51b66b23a28807cc7b67839a9eb3dee57d3
alisa69 <aliska132007@gmail.ru>
2025-11-29 00:48:40 +0500

    Task5

---
+++ Tyuiu.RadchenkoAV.Sprint5.Task5.V9.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RadchenkoAV.Sprint5.Task5.V9.Lib
+{
+    public class DataService : ISprint5Task5V9
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string fileContent = File.ReadAllText(path);
+            var values = fileContent
+                .Split(new[] { ' ', '\n', '\r', ',', ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(val =>
+                {
+                    if (double.TryParse(val, out double doubleValue))
+                    {
+                        return Math.Round(doubleValue, 3);
+                    }
+                    return (double?)null;
+                })
+                .Where(val => val.HasValue)
+                .Select(val => val.Value)
+                .ToArray();
+            var maxInteger = values.Where(val => val == Math.Truncate(val)).Max();
+            return maxInteger;
+        }
+    }
+}
---
+++ Tyuiu.RadchenkoAV.Sprint5.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Tyuiu.RadchenkoAV.Sprint5.Task5.V9.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint5.Task5.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask5V9.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
